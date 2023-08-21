1ee537f7b65f4419541a88cf0f4dbaecfc8422b9
Rabya Tayal <rabyatayal1201@gmail.com>
2023-08-21 18:52:51 +1000

    Basic Expense Tracker

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,67 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
+  void _addExpenseToList(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) => NewExpense(
+              addExpenseToList: _addExpenseToList,
+            ));
+  }
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: const Text('Flutter ExpenseTracker'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _openAddExpenseOverlay,
+          )
+        ],
+      ),
+      body: Column(children: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text('Expenses Chart'),
+        ),
+        Expanded(
+          child: ExpensesList(expenses: _registeredExpenses),
+        )
+      ]),
+    );
+  }
+}
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({required this.addExpenseToList, super.key});
+  final void Function(Expense expense) addExpenseToList;
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _expenseAmountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _expenseAmountController.dispose();
+    super.dispose();
+  }
+
+  void _submitExpense() {
+    final enteredAmount = double.tryParse(_expenseAmountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount < 0;
+    if (_titleController.text.isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      //throw error
+      showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text('Invalid input'),
+                content: const Text(
+                    'Please make sure a valid title, amount, date and country are entered'),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(ctx);
+                      },
+                      child: const Text('Ok'))
+                ],
+              ));
+      return;
+    }
+
+    Expense newExpense = Expense(
+        amount: enteredAmount,
+        category: _selectedCategory,
+        date: _selectedDate!,
+        title: _titleController.text);
+
+    widget.addExpenseToList(newExpense);
+    Navigator.pop(context);
+  }
+
+  Future<void> _presentDatePicker(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now().subtract(const Duration(days: 1)),
+      lastDate: DateTime.now().add(const Duration(days: 1095)),
+    );
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(children: [
+        TextField(
+          controller: _titleController,
+          maxLength: 50,
+          decoration: const InputDecoration(label: Text('Title')),
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _expenseAmountController,
+                maxLength: 20,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  prefixText: '\$',
+                  label: Text('Expense Amount'),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Expanded(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(_selectedDate == null
+                    ? 'No selected date'
+                    : DateFormat.yMd().format(_selectedDate!)),
+                IconButton(
+                    onPressed: () async {
+                      await _presentDatePicker(context);
+                    },
+                    icon: const Icon(Icons.calendar_month))
+              ],
+            ))
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            DropdownButton(
+              value: _selectedCategory,
+              items: Category.values
+                  .map((category) => DropdownMenuItem(
+                      value: category,
+                      child: Text(category.name.toUpperCase())))
+                  .toList(),
+              onChanged: (value) => setState(() {
+                if (value == null) {
+                  return;
+                }
+                _selectedCategory = value;
+              }),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _submitExpense();
+                },
+                child: const Text('Save Expense'))
+          ],
+        )
+      ]),
+    );
+  }
+}
